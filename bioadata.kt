afb3a85370535e497e7e58a51d07974f4371726d
bidadari kehilangan selendang <145539450+achiyaav@users.noreply.github.com>
2023-09-20 15:07:20 +0700

    Create bioadata.kt

---
+++ bioadata.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+  var nama = "Melias Divalova"
+  var kelas = "XI RPL 2"
+  var hobi = "berenang dan memasak"
+
+  println("Nama: $nama") 
+  println("Kelas: $kelas")
+  println("Hobi: $hobi")
+}
